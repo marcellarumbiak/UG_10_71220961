3bffe797a2c1fb72b165cbab0bccb96ad469f3d1
marcellarumbiak <117884708+marcellarumbiak@users.noreply.github.com>
2022-11-24 13:15:15 +0700

    Add files via upload

---
+++ 1_B_71220961.py
@@ -0,0 +1,7 @@
+print("Program Penghitung Volume Bangun Ruang")
+panjang = input("Masukkan panjang sisi alas limas : ")
+tinggi = input("Masukkan tinggi limas: ")
+# Masukkan output
+print("1/3" * panjang * tinggi)
+
+
